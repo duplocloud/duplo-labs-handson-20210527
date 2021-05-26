03acd7a6fceeb610beedb04b62ab94fb0193a67b
Joe Khoobyar <fourheads@gmail.com>
2021-05-26 15:15:59 -0400

    steps 1 through 5:
      - STEP 1 - Declare Terraform and provider versions
      - STEP 2 - Configure DuploCloud provider
      - STEP 3 - Retrieve information about the base infrastructure that was previous created
      - STEP 4 - Create tenant for the lab session
      - STEP 5 - Create and harden multiple AWS managed services that the application will use

---
+++ aws-services.tf
@@ -0,0 +1,58 @@
+// STEP 5 - Create and harden multiple AWS managed services that the application will use:
+//
+//   - an AWS ElasticCache Redis instance
+//   - an AWS Kafka cluster
+//   - an AWS S3 bucket
+//   - an AWS RDS instance (with a randomly generated password)
+//
+
+resource "duplocloud_ecache_instance" "mycache" {
+  tenant_id  = duplocloud_tenant.this.tenant_id
+  name       = "mycache"
+  cache_type = 0 // Redis
+  replicas   = 1
+  size       = "cache.t2.small"
+}
+
+resource "duplocloud_aws_kafka_cluster" "this" {
+  tenant_id     = duplocloud_tenant.this.tenant_id
+  name          = "mycluster"
+  kafka_version = "2.4.1.1"
+  instance_type = "kafka.m5.large"
+  storage_size  = 20
+  timeouts {
+    create = "45m"
+  }
+}
+
+resource "duplocloud_s3_bucket" "mydata" {
+  tenant_id = duplocloud_tenant.this.tenant_id
+  name      = "mydata"
+
+  allow_public_access = false
+  enable_access_logs  = true
+  enable_versioning   = true
+  managed_policies    = ["ssl"]
+  default_encryption {
+    method = "Sse"
+  }
+}
+
+resource "duplocloud_rds_instance" "this" {
+  tenant_id      = duplocloud_tenant.this.tenant_id
+  name           = "psql"
+  engine         = 1 // PostgreSQL
+  engine_version = "12.5"
+  size           = "db.t3.medium"
+
+  master_username = "lab"
+  master_password = random_password.mypassword.result
+
+  encrypt_storage = true
+}
+
+resource "random_password" "mypassword" {
+  length  = 16
+  number  = true
+  special = false
+}
---
+++ main.tf
@@ -0,0 +1,39 @@
+// STEP 1 - Declare Terraform and provider versions.
+terraform {
+  required_version = "~> 0.14"
+  required_providers {
+    duplocloud = {
+      version = "~> 0.5.33"
+      source  = "duplocloud/duplocloud"
+    }
+    random = { version = "~> 3.1.0" }
+  }
+}
+
+// STEP 2 - Configure DuploCloud provider.
+provider "duplocloud" {
+  # duplo_host = "https://labstest01.duplocloud.net"
+  # duplo_token = "xxx"
+}
+
+// STEP 3 - Retrieve information about the base infrastructure that was previous created.
+data "duplocloud_aws_account" "this" {}
+data "duplocloud_admin_aws_credentials" "this" {}
+data "terraform_remote_state" "base-infra" {
+  backend = "s3"
+
+  config = {
+    region     = "us-west-2" # bucket region
+    bucket     = "duplo-tfstate-${data.duplocloud_aws_account.this.account_id}"
+    key        = "infra:/lab/base-infra"
+    access_key = data.duplocloud_admin_aws_credentials.this.access_key_id
+    secret_key = data.duplocloud_admin_aws_credentials.this.secret_access_key
+    token      = data.duplocloud_admin_aws_credentials.this.session_token
+  }
+}
+
+// STEP 4 - Create tenant for the lab session.
+resource "duplocloud_tenant" "this" {
+  account_name = "lab"
+  plan_id      = data.terraform_remote_state.base-infra.outputs["plan_id"]
+}
